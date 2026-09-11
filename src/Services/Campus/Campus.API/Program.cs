5d61881bc6ed811e3aa2be58d2c132b8e8ccdd93
Nikoooo <ncayuman2025@alu.uct.cl>
2026-09-11 01:45:45 -0300

    feat: configuración inicial de PostgreSQL y Entity Framework

--- src/Services/Campus/Campus.API/Program.cs
+++ src/Services/Campus/Campus.API/Program.cs
@@ -1,5 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using Campus.Infraestructure.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<CampusDbContext>(options =>
+        options.UseNpgsql(builder.Configuration.GetConnectionString("CampusDb")));
+
 var app = builder.Build();
+
+app.Run();
